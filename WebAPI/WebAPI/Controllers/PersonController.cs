0a9db5c468669b93f7823f29e16b6bc42ecfcf52
poyingHAHA <poying.code@gmail.com>
2022-07-29 11:01:44 +0800

    WebAPI2 - 異步與HTTP參數傳遞

---
+++ WebAPI/WebAPI/Controllers/PersonController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Person> GetAll()
+        {
+            Person p1 = new Person("Edwin", 18);
+            Person p2 = new Person("John", 20);
+            IEnumerable<Person> people = new Person[] { p1, p2 };
+            return people;
+        }
+
+        [HttpGet]
+        public Person GetPersonById(int id)
+        {
+            if(id == 1)
+            {
+                return new Person("Waku", 5);
+            }
+            else if(id == 2)
+            {
+                return new Person("Lora", 10);
+            }
+            return null;
+        }
+
+        [HttpPost]
+        public string AddNew(Person p)
+        {
+            return "搞定";
+        }
+    }
+}
---
+++ WebAPI/WebAPI2_async/Controllers/HelloController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI2_async.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public int Add(int i, int j)
+        {
+            return i + j;
+        }
+
+        [HttpGet]
+        public async Task<string> Add2()
+        {
+            string s = await System.IO.File.ReadAllTextAsync(@"E:\Dotnet_a-z\Dotnet_A-Z-2_ASPNETCOR\WebAPI\WebAPI2_async\test.txt");
+            return s.Substring(0, 20);
+        }
+
+        [HttpGet]
+        public Person Test2()
+        {
+            return new Person {
+                Id = 5,
+                Name = "edwin",
+                Children = new Child[] { new Child { Name = "aaa" }, new Child { Name = "bbb" } }
+            };
+        }
+
+        [HttpGet("{i}/{j}")]
+        public int Test3(int i, int j)
+        {
+            return i + j;
+        }
+
+        [HttpGet("{i}/{j}")]
+        public int Test4([FromRoute(Name ="i")] int x, int j)
+        {
+            return x * j;
+        }
+
+        [HttpPost]
+        public string AddPerson(Person p)
+        {
+            return "Saved " + p.Id + ". " + p.Name;
+        }
+
+        [HttpPut("{id}")]
+        public string UpdatePerson([FromRoute] int id, Person p)
+        {
+            return "update " + id + " success " + p.Name;
+        }
+    }
+    public class Child
+    {
+        public string Name { get; set; }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public Child[] Children { get; set; }
+    }
+
+}
