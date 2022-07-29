6f3ac98412488e8692dd67758c498e48fe970869
poyingHAHA <poying.code@gmail.com>
2022-07-29 20:25:36 +0800

    WebAPI15 - 為緩存設置超時AbsoluteExpirationRelativeToNo與SlidingExpiration

--- WebAPI/Cache1/Controllers/TestController.cs
+++ WebAPI/Cache1/Controllers/TestController.cs
@@ -20,18 +20,13 @@ namespace Cache1.Controllers
         [HttpGet]
         public async Task<ActionResult<Book>> GetBookByIdAsync(long id)
         {
-            //Book? result = MyDbContext.GetById(id);
-            //if(result == null)
-            //{
-            //    return NotFound($"{id} not found");
-            //}
-            //return Ok(result);
-
             Console.WriteLine("開始執行");
 
             // 1) 從緩存取數據 2)沒有的話到資料源取數據
             Book? b = await memoryCache.GetOrCreateAsync("Book"+id, async(e) =>{
                 Console.WriteLine("還存沒有，到數據庫查");
+                // e.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(10); // 緩存10秒
+                e.SlidingExpiration = TimeSpan.FromSeconds(10);
                 return await MyDbContext.GetByIdAsync(id);
             });
 
